96b6b15b2e4feee7ea03103d0bb277f4a2f514de
Glendson <gztomazetto@gmail.com>
2024-09-04 16:02:07 +0100

    feat: finishing tests on cache and fix issues in repository

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -94,7 +94,7 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
     if (cacheHit) {
       const cachedData = JSON.parse(cacheHit)
 
-      return cachedData
+      return PrismaQuestionDetailsMapper.toDomain(cachedData)
     }
 
     const question = await this.prisma.question.findUnique({
@@ -109,12 +109,12 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
 
     if (!question) return null
 
-    const questionDetails = PrismaQuestionDetailsMapper.toDomain(question)
-
     await this.cacheRepository.set(
       `questions:${slug}:details`,
-      JSON.stringify(questionDetails),
+      JSON.stringify(question),
     )
+
+    const questionDetails = PrismaQuestionDetailsMapper.toDomain(question)
 
     return questionDetails
   }
